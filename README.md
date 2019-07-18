f2c94c6d5dfeb726babdab7103ab2fde9f8fd0cb
Leandro Diniz <leandro.diniz@outlook.com.br>
2019-07-18 03:24:53 -0300

    feat(lib): remove app folder and add repository url

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NgxDocumentsValidators
+# Angular Documents Validators Libary
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
 
--- src/app/certidao/directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CertificateValidator } from './directive';
-
-describe('CertificateValidator', () => {
-  it('should create an instance', () => {
-    const directive = new CertificateValidator();
-    expect(directive).toBeTruthy();
-  });
-});
